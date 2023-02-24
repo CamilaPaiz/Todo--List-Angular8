d86ef5aeb09166d47f9d6140b37c0acba145d341
CamilaPaiz <camilapaiz@hotmail.com>
2023-02-24 20:28:03 -0300

    fix: botão cancel removido e alterações na exibição do botão edit e save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,11 +17,12 @@ interface Task {
 export class AppComponent {
   title = 'To-do List';
   tasks: Task[] = [];
-  newTask:'';
-  editingTask: Task | null = null;
+  newTask = '';
+  editingTaskId: number | null = null;
   isSubmitting = false;
   
   constructor(private http: HttpClient) {}
+  
   ngOnInit() {
     this.http.get<Task[]>('http://127.0.0.1:8000/todo/').subscribe(data => {
       this.tasks = data;
@@ -39,30 +40,22 @@ export class AppComponent {
   
   removeTask(id: number) {
     this.http.delete(`http://127.0.0.1:8000/todo/${id}/`).subscribe(() => {
-        this.tasks = this.tasks.filter(task => task.id !== id);
+      this.tasks = this.tasks.filter(task => task.id !== id);
     });
   }
 
   editTask(id: number) {
-    this.editingTask = this.tasks.find(task => task.id === id);
+    this.editingTaskId = id;
   }
   
-  updateTask() {
-    if (this.editingTask) {
-      this.http.put<Task>(`http://127.0.0.1:8000/todo/${this.editingTask.id}/`, this.editingTask).subscribe(task => {
-        const index = this.tasks.findIndex(task => task.id === this.editingTask.id);
-
-        if (index >= 0) {
-          this.tasks[index] = task;
-        }
-        this.editingTask = null;
-      });
-    }
-  }
-  
-  
-  cancelEdit() {
-    this.editingTask = null;
+  updateTask(task: Task) {
+    this.http.put<Task>(`http://127.0.0.1:8000/todo/${task.id}/`, task).subscribe(() => {
+      const index = this.tasks.findIndex(t => t.id === task.id);
+      if (index >= 0) {
+        this.tasks[index] = task;
+      }
+      this.editingTaskId = null;
+    });
   }
   
   markDone(id: number, done: boolean) {
@@ -75,5 +68,5 @@ export class AppComponent {
       });
     });
   }
-  
 }
+
