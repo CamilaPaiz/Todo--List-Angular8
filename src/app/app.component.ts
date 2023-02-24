fd3212701c5648ba9493dcca112b9487713c0936
CamilaPaiz <camilapaiz@hotmail.com>
2023-02-24 15:01:59 -0300

    feat:implementando interface Task em app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+interface Task {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'todo-list';
+  tasks: Task[] = [];
 }
